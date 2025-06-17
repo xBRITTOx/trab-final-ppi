a665b35aa256c7dbe53a314799b25d71e7ff1fa7
Cristiano Ferreira da Silva Brito <criis.briito@hotmail.com>
2025-06-17 18:06:07 -0300

    2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
+import express from "express";
+import cookieParser from "cookie-parser";
 const app = express();
 const port = 3000;
 const host = "localhost";
